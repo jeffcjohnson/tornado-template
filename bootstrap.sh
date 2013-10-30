f02833d966bbcfc8acee332f582ee35f9f488e18
Jeff Johnson <jeffcjohnson@gmail.com>
2013-10-30 16:37:34 -0400

    make this work in OSX with homebrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "s/SITENAME/${1}/g" setup.py SITENAME/*.py SITENAME/models/__init__.py SITENAME/handlers/__init__.py
+sed -i -e "s/SITENAME/${1}/g" setup.py SITENAME/*.py SITENAME/models/__init__.py SITENAME/handlers/__init__.py
 
 if [[ -d "SITENAME" ]]; then
     mv SITENAME $1
@@ -10,9 +10,9 @@ if [[ -a "README.md" ]]; then
     echo -e "# $1\n\nPlease write a useful readme." > README.md
 fi
 
-if [[ -x "/usr/bin/virtualenvwrapper.sh" ]]; then
-    source /usr/bin/virtualenvwrapper.sh
-    mkvirtualenv -p python3.3 $1
+if [[ -x "/usr/local/bin/virtualenvwrapper.sh" ]]; then
+    source /usr/local/bin/virtualenvwrapper.sh
+    mkvirtualenv -p /usr/local/bin/python3.3 $1
     pip install -r requirements.txt
 else
     echo "No virtualenvwrapper.sh found. No virtualenv created!"
@@ -22,6 +22,6 @@ rm -rf .git
 git init .
 
 cd ../
-mv tornado-template ${1^}
-cd ${1^}
+mv tornado-template ${1}
+cd ${1}
 rm bootstrap.sh
